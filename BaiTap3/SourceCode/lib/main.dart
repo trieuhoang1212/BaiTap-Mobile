6d011ddf73aa71e1e6085b7efc20c228011181e9
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-12-14 22:51:13 +0700

    COMPLETE: BaiTap3 và viết readme

--- BaiTap3/SourceCode/lib/main.dart
+++ BaiTap3/SourceCode/lib/main.dart
@@ -26,6 +26,47 @@ class CalculatePage extends StatefulWidget {
 }
 
 class _CalculatePage extends State<CalculatePage> {
+  final TextEditingController _controllerA = TextEditingController();
+  final TextEditingController _controllerB = TextEditingController();
+  String _result = '';
+
+  void _calculate(String operator) {
+    double? a = double.tryParse(_controllerA.text);
+    double? b = double.tryParse(_controllerB.text);
+    if (a == null || b == null) {
+      setState(() {
+        _result = 'Không hợp lệ';
+      });
+      return;
+    }
+    double res;
+    switch (operator) {
+      case '+':
+        res = a + b;
+        break;
+      case '-':
+        res = a - b;
+        break;
+      case '*':
+        res = a * b;
+        break;
+      case '/':
+        if (b == 0) {
+          setState(() {
+            _result = 'Không thể chia cho 0';
+          });
+          return;
+        }
+        res = a / b;
+        break;
+      default:
+        res = 0;
+    }
+    setState(() {
+      _result = res.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,10 +86,11 @@ class _CalculatePage extends State<CalculatePage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: TextField(
+                controller: _controllerA,
                 textAlign: TextAlign.left,
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
-                  hintText: 'Nhập giá trị A',
+                  hintText: 'Nhập giá trị A ',
                   border: OutlineInputBorder(),
                   contentPadding: EdgeInsets.symmetric(
                     vertical: 15,
@@ -62,7 +104,91 @@ class _CalculatePage extends State<CalculatePage> {
               ),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: const Text('+')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll<Color>(
+                      const Color.fromARGB(255, 0, 131, 239),
+                    ),
+                    foregroundColor: WidgetStatePropertyAll<Color>(
+                      Colors.white,
+                    ),
+                  ),
+                  onPressed: () => _calculate('+'),
+                  child: const Text('+'),
+                ),
+
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll<Color>(
+                      const Color.fromARGB(255, 182, 0, 0),
+                    ),
+                    foregroundColor: WidgetStatePropertyAll<Color>(
+                      Colors.white,
+                    ),
+                  ),
+                  onPressed: () => _calculate('-'),
+                  child: const Text('-'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll<Color>(
+                      const Color.fromARGB(255, 58, 137, 1),
+                    ),
+                    foregroundColor: WidgetStatePropertyAll<Color>(
+                      Colors.white,
+                    ),
+                  ),
+                  onPressed: () => _calculate('*'),
+                  child: const Text('*'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () => _calculate('/'),
+                  child: const Text('/'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: _controllerB,
+                textAlign: TextAlign.left,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Nhập giá trị B',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 12,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 12,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Kết quả: $_result',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 0, 21, 103),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- BaiTap3/TaiLieu/README.md
+++ BaiTap3/TaiLieu/README.md
@@ -1,19 +1,29 @@
 # **Mô Tả**
-Bài tập số 2 này làm về giao diện nhập số nguyên dương, nhập số liệu thì sẽ tạo dãy số từ 1 đến số người dùng nhập. Nếu nhập sai khi phải là số dương hay là kí tự là chữ cái hoặc đặt biệt thì sẽ không hiển thị dãy mong muốn.
+
+Bài tập 2 yêu cầu sinh viên tạo 1 giao diện đơn giản cho phép người dùng có thể nhập 2 giá trị a và b, có thể chọn phép tính và được hiện kết quả.
 
 ![NhapDung](../SourceCode/assets/images/UI_mau.jpg)
+
 # **Các Hàm Chính**
+
 **Main:** khởi tạo flutter
 **MyApp.build:** cấu hình theme và thiết lập NumberList
 **createList:** đọc số nguyên từ TextField, kiểm tra hợp lệ, hiển thị lỗi, tạo danh sách từ 1 đến n.
-**_NumberListState.build():** TextField để nhập số, nút "Tạo" để tạo createList().
+**\_NumberListState.build():** TextField để nhập số, nút "Tạo" để tạo createList().
 
 # **Kết Quả**
-Giao diện:
-![NhapDung](../SourceCode/assets/images/giao_dien_chinh.png)
 
-Nhập đúng cứu pháp:
-![NhapDung](../SourceCode/assets/images/nhap_dung_cuu_phap.png)
-
-Nhập sai cứu pháp:
-![NhapDung](../SourceCode/assets/images/nhap_sai_cuu_phap.png)
+**Giao diện:**
+    ![NhapDung](../SourceCode/assets/images/giao_dien_chinh.png)
+**Nhập đúng cứu biểu thức cộng:**
+    ![NhapDung](../SourceCode/assets/images/nhap_gia_tri_cong.png)
+**Nhập đúng cứu biểu thức trừ:**
+    ![NhapDung](../SourceCode/assets/images/nhap_gia_tri_cong.png)
+**Nhập đúng cứu biểu thức nhân:**
+    ![NhapDung](../SourceCode/assets/images/nhap_gia_tri_cong.png)
+**Nhập đúng cứu biểu thức chia:**
+    ![NhapDung](../SourceCode/assets/images/nhap_gia_tri_chia.png)
+**Nhập sai cứu pháp:**
+    ![NhapDung](../SourceCode/assets/images/sai_cuu_phap.png)
+**Điều kiện chia:**
+    ![NhapDung](../SourceCode/assets/images/dieu_kien_chia.png)
